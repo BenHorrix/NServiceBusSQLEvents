966f80725df88282f7e8b32b930618660f4d604e
Ben Horrix <ben.horrix@jet2.com>
2018-06-01 11:43:11 +0100

    Added SQL persistence

--- Shared/ConfigurationHelper.cs
+++ Shared/ConfigurationHelper.cs
@@ -1,20 +1,29 @@
 ﻿namespace Shared
 {
+    using System.Data.SqlClient;
     using NServiceBus;
     using NServiceBus.Features;
+    using NServiceBus.Persistence.Sql;
 
     public static class ConfigurationHelper
     {
         public static EndpointConfiguration GetSqlConfiguration(string name)
         {
+            const string connectionString = "Data Source=localhost;Initial Catalog=Messages;User ID=messages_user;Password=password123";
             var endpointConfiguration = new EndpointConfiguration(name);
 
             endpointConfiguration.EnableInstallers();
             endpointConfiguration.DisableFeature<TimeoutManager>();
-            endpointConfiguration.UsePersistence<InMemoryPersistence>();
+            var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+            persistence.SqlDialect<SqlDialect.MsSqlServer>();
+            persistence.ConnectionBuilder(
+                connectionBuilder: () =>
+                {
+                    return new SqlConnection(connectionString);
+                });
 
             var transport = endpointConfiguration.UseTransport<SqlServerTransport>();
-            transport.ConnectionString("Data Source=localhost;Initial Catalog=Messages;User ID=messages_user;Password=password123");
+            transport.ConnectionString(connectionString);
             transport.Transactions(TransportTransactionMode.None);
 
             return endpointConfiguration;
